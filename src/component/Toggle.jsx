a14bba0bdf1d3c8880b4f61359b6c2cb6499decb
hjhsu <hjhsu@gmail.com>
2019-11-07 09:39:58 +0800

    修改錯誤

--- src/component/Toggle.jsx
+++ src/component/Toggle.jsx
@@ -4,19 +4,17 @@ export default class Toggle extends Component {
     constructor(props) {
         super(props)
         this.state = {
-             isOpen:true,
+            isOpen: true,
         }
     }
     handleClick = () => {
-        const {Add} =this.props
         this.setState({isOpen:!this.state.isOpen})
-        Add()
-      }
+    }
     render() {
-      
+
         return (
             <div className="toggle">
-                {this.state.isOpen?"😸":"😹"}
+                {this.state.isOpen ? "😸" : "😹"}
                 <button onClick={this.handleClick}>切換</button>
             </div>
         )
